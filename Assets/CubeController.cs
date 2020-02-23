c0a183a7930785d42478d55a3e22c14476a55ff7
SouichirouTakahashi2 <ar.hickers@gmail.com>
2020-02-23 18:54:12 +0900

    Lesson7の課題　「ブロックが積みあがる音をいれる」を行っています。
    　現在「awake」に　チェックを入れていますので　最初の「ゴトッ」というのみ　音が出ますが　後は全く続きません。　何が足らないのでしょうか？　codeは CubeContorollerに入れています。
    
    if (collision.gameObject.name == "CubePrefab")
    
            {   //block音を出す
                GetComponent<AudioSource>().Play();
            }
        }
    
        // Use this for initialization
        void Start()
        {
            AudioSource BlockSound = GetComponent<AudioSource>();
        }

---
+++ Assets/CubeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    //キューブの移動速度
+    private float speed = -12;
+
+    //消滅位置
+    private float deadLine = -10;
+
+    private bool BlockSound;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //衝突判定
+        if (collision.gameObject.name == "CubePrefab")
+
+        {   //block音を出す
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioSource BlockSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        //画面外にでたら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    }
+
+
+
+
+
+
